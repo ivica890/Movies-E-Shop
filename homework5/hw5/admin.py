0c76a04116c01d2d10528234e3b2269cd4de84d4
Ivica Cickoski <icickovski@gmail.com>
2023-06-19 03:47:16 +0200

    admin panel permissions created

--- homework5/hw5/admin.py
+++ homework5/hw5/admin.py
@@ -5,9 +5,35 @@ from .models import Film, Seat, Order
 class FilmAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
 
 class SeatAdmin(admin.ModelAdmin):
     list_display = ['seat_number']
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_view_or_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
 
 
 class OrderAdmin(admin.ModelAdmin):
